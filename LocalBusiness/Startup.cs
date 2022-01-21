34bf37ca4a6343b328f2ebe68bc76c0e31aeba7d
Kate Kiatsiri <keidsiri@gmail.com>
2022-01-21 09:46:47 -0800

    Update Startup.cs file

--- LocalBusiness/Startup.cs
+++ LocalBusiness/Startup.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using LocalBusiness.Models;
 using Microsoft.OpenApi.Models;
+using System;
 
 namespace LocalBusiness
 {
@@ -27,24 +23,53 @@ namespace LocalBusiness
         public void ConfigureServices(IServiceCollection services)
         {
 
+            
+            services.AddDbContext<LocalBusinessContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            //Swagger
+            services.AddSwaggerGen(options =>
+                {
+            options.SwaggerDoc("v1", new OpenApiInfo
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LocalBusiness", Version = "v1" });
+                Version = "v1",
+                Title = "LocalBusiness API",
+                Description = "An ASP.NET Core Web API for managing List of Local Business like Restaurants and Shops",
+                TermsOfService = new Uri("https://example.com/terms"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Example Contact",
+                    Url = new Uri("https://example.com/contact")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Example License",
+                    Url = new Uri("https://example.com/license")
+                }
             });
+        });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalBusiness v1"));
             }
 
             // app.UseHttpsRedirection();
+
+            //Swagger 
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
